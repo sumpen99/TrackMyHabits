45b18d1ac6631e57af6235d2dc58d6e60796d76e
sumpen99 <sumpen841001@hotmail.com>
2023-04-17 16:26:44 +0200

    Initial Commit

---
+++ TrackMyHabits/TrackMyHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackMyHabitsApp.swift
+//  TrackMyHabits
+//
+//  Created by fredrik sundström on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct TrackMyHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
